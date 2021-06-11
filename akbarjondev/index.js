0b246ba0ec5b36b651ea04eb4a8ed897c1a99335
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-06-11 11:52:43 +0500

    Fix BigInt error

--- akbarjondev/index.js
+++ akbarjondev/index.js
@@ -1,3 +1,3 @@
-const main = arr => String(arr.join('') - 0 + 1).split('')
+const main = arr => String(BigInt(arr.join('')) + BigInt(1)).split('')
 
-main([0])
+console.log(main([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
